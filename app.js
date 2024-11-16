e3c33697754e94b9cada7c445a00f7cac6fd0789
Miguel <l21070008@huetamo.tecnm.mx>
2024-11-16 14:36:08 -0600

    Cambio al app.js

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ require('dotenv').config();
 // Conexión a MongoDB
  //conexion local   /*mongoose.connect('mongodb://localhost:27017/CatalogoAlumnos', {  */
  mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 })
 .then(() => {
   console.log('Conexión exitosa a MongoDB');
